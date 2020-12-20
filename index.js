a3fff616dff2fcaf4abfc2ccb060f290e47f6927
unknown <peters0470@gmail.com>
2020-12-20 17:06:49 -0600

    added database

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const inquirer= require("inquirer");
 const cTable = require("console.table");
 const mysql = require("mysql2");
 
-var connection = mysql.createConnection({
+const connection = mysql.createConnection({
     host: "localhost", 
     port: 3306,
     user: "root",
